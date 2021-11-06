4ef81f632935a1b5020f6bc468ab277098eaae36
hugosenari <hugosenari@gmail.com>
2021-11-06 01:48:48 -0300

    chore: hide complexity

--- template/flake.nix
+++ template/flake.nix
@@ -3,5 +3,5 @@
 
   inputs.dsf.url = "github:cruel-intentions/devshell-files";
 
-  outputs = in: in.dsf.lib.mkShell [ ./project.nix ];
+  outputs = inputs: inputs.dsf.lib.mkShell [ ./project.nix ];
 }
