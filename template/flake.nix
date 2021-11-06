9bcf8aeb65aa2d0e765f3d638181472828dc3897
hugosenari <hugosenari@gmail.com>
2021-11-06 01:42:08 -0300

    chore: hide complexity

--- template/flake.nix
+++ template/flake.nix
@@ -1,9 +1,7 @@
 {
   description = "Dev Environment";
 
-  inputs.devshell-files.url = "github:cruel-intentions/devshell-files";
+  inputs.dsf.url = "github:cruel-intentions/devshell-files";
 
-  outputs = { self, devshell-files, nixpkgs }: devshell-files.lib.mkShell [
-    ./my-project-module.nix
-  ];
+  outputs = deps: deps.dsf.lib.mkShell [ ./project.nix ];
 }
