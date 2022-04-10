06fe9196da9a1e44efc90d1ad0135a2cd827fa5d
hugosenari <hugosenari@gmail.com>
2022-04-10 13:54:56 -0300

    docs: example serving gh-pages

--- examples/services.nix
+++ examples/services.nix
@@ -6,7 +6,7 @@
   # by default httplz will start at .data/services/httplz
   # use an alias to configure it properly
   # if you don't add `exec` it may not receive stop command
-  files.alias.httpd    = "exec httplz --port 8022 $PRJ_ROOT";
+  files.alias.httpd    = "exec httplz --port 8022 $PRJ_ROOT/gh-pages/book/";
   files.services.httpd = true;
   # Make S6 start when you enter in shell
   # files.services.s6    = true;
