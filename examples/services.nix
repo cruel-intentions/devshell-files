7b943fc0aefd82fa5f9702402d6bd4cb2409f481
hugosenari <hugosenari@gmail.com>
2022-04-26 03:54:04 -0300

    feat(services): add notification example

--- examples/services.nix
+++ examples/services.nix
@@ -1,40 +1,45 @@
 {
-  # Enable httplz to your shell
-  files.cmds.httplz    = true;
+  # # Make services start when you enter in shell
+  # files.services.initSrvs = true;
 
-  # Use httplz as service
-  # by default httplz will start at .data/services/httplz
-  # use an alias to configure it properly
-  # if you don't add `exec` it may not receive stop command
+  # static http service
+  files.cmds.httplz    = true; # Install httplz
+  files.services.httpd = true; # Use cmd 'httpd' as service
   files.alias.httpd    = ''
+    # This is an alias of httplz
     cd $PRJ_ROOT/gh-pages
     mdbook build
     httplz --port 8022 $PRJ_ROOT/gh-pages/book/
   '';
-  files.services.httpd = true;
-  # Make services start when you enter in shell
-  # files.services.initSrvs = true;
 
   # greeting service
+  files.cmds.notify-desktop = true; # Install notify-desktop
+  files.services.greet      = true; # Use cmd 'greet' as service
   files.alias.greet         = ''
-    notify-desktop -i network-server "Services Started";
-    # if our service ends it will be restarted
-    # so we sleep forever
-    sleep infinity
-  '';
-  files.alias.greet-finish  = ''notify-desktop -i network-server "Services Stoped" '';
-  files.services.greet      = true;
-  files.cmds.notify-desktop = true;
+    # Greet people
+    notify-desktop -i network-server "Starting services";
 
-  # RC is another interface it uses s6-rc
+    echo Dornröschen  # our service stdout goes to .data/log/greet
+    sleep infinity    # any service should run infinitely
+  '';
+  files.alias.greet-finish  = ''
+    notify-desktop -i network-server "Stoping services"
+  '';
+
+
+  # # RC (unstable) is another interface it uses s6-rc
+  # # http://skarnet.org/software/s6-rc/why.html
+  #
   # files.rc.hello.enable  = true;
   # files.rc.hello.oneshot.start    = ''wait 1000000 "hello world"'';
   # files.rc.hello.oneshot.stop     = ''echo "bye bye world"'';
   # files.rc.hello.oneshot.timeout  = 1200;
+  #
   # files.rc.olla.enable   = true;
   # files.rc.olla.longrun.start     = ''echo "hello world"'';
   # files.rc.olla.longrun.stop      = ''echo "bye bye world"'';
   # files.rc.olla.longrun.deps      = ["hello"];
+  #
   # files.rc.hellos.enable = true;
   # files.rc.hellos.bundle.deps     = ["hello" "olla"];
 }
