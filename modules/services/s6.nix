e24fb6adebb1ec3696359efae60a671a041ecb30
hugosenari <hugosenari@gmail.com>
2022-04-30 04:48:53 -0300

    fix(services): propperlogins system

--- modules/services/s6.nix
+++ modules/services/s6.nix
@@ -1,10 +1,9 @@
 let
-  exclib   = import ./execline.nix;
+  exclib = import ./execline.nix;
 in
 rec {
   log     = { name, flags ? "" }: with exclib; ''
     ${loadEnv "PRJ_SVCS_LOG"}
-      ${errToOut}
       s6-log ${flags} "''${PRJ_SVCS_LOG}/${name}"'';
   scan    = { flags ? "" }: with exclib; ''
     ${loadEnv "PRJ_SVCS_DIR"}
@@ -15,19 +14,11 @@ rec {
       ${errToOut}
       s6-svscanctl ${flags} ''${PRJ_SVCS_DIR}'';
   scanAndLog = { scanFlags ? "", logFlags ? "-b n4 s100000" }: with exclib; ''
-    ${loadEnv "PRJ_SVCS_LOG"}
-    ${loadEnv "PRJ_SVCS_DIR"}
-
-    ${redirfd "\${PRJ_SVCS_LOG}/svscan/log"}
-    ${errToOut}
-
-    s6-svscan ${scanFlags} ''${PRJ_SVCS_DIR}
+    ${pipeline (scan { flags = scanFlags; })}
+    ${log { name = "scan"; flags = logFlags;}}
   '';
   scanCtlAndLog = { ctlFlags ? "", logFlags ? "-b n4 s100000" }: with exclib; ''
-    ${loadEnv "PRJ_SVCS_LOG"}
-    ${loadEnv "PRJ_SVCS_DIR"}
-    ${redirfd "\${PRJ_SVCS_LOG}/svscanctl/log"}
-    ${errToOut}
-    s6-svscanctl ${ctlFlags} ''${PRJ_SVCS_DIR}
+    ${pipeline (scanCtl { flags = ctlFlags; })}
+    ${log { name = "scanCtl"; flags = logFlags; }}
   '';
 }
