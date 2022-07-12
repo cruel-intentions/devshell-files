04527fd1f5caac91b52f019998b766c5f4267c0a
hugosenari <hugosenari@gmail.com>
2022-07-12 01:39:33 -0300

    chore: changing some code style

--- modules/file-format.nix
+++ modules/file-format.nix
@@ -1,31 +1,27 @@
 format: {pkgs, config, lib, ...}: 
 let
+  yj-args.hcl  = "-jc";
   yj-args.json = "-jji";
-  yj-args.yaml = "-jy";
   yj-args.toml = "-jt";
-  yj-args.hcl = "-jc";
+  yj-args.yaml = "-jy";
   yj-arg = yj-args.${format};
-  cfg = config.files.${format};
-  type = (pkgs.formats.json {}).type;
-  generate = name: value: pkgs.runCommand name {
-      nativeBuildInputs = [ pkgs.yj ];
-      value = builtins.toJSON value;
-      passAsFile = [ "value" ];
-    } ''
-      cat "$valuePath"| yj ${yj-arg} > "$out"
-    '';
+  cfg    = config.files.${format};
+  type   = (pkgs.formats.json {}).type;
+  gen    = name: value: pkgs.runCommand ".dsf${lib.strings.sanitizeDerivationName name}" {
+    nativeBuildInputs = [ pkgs.yj ];
+    passAsFile        = [ "value" ];
+    value             = builtins.toJSON value;
+  } ''yj ${yj-arg} < $valuePath > $out'';
   toFile = name: value: {
-    source = generate (builtins.baseNameOf name) value;
+    source  = gen name value;
     git-add = lib.mkIf config.files.git.auto-add true;
   };
 in {
   options.files.${format} = lib.mkOption {
-    type = lib.types.attrsOf type;
-    description = ''
-      Create ${format} files with correponding content
-    '';
-    default = {};
-    example."/hello.${format}".greeting = "hello World";
+    default     = {};
+    description = ''Create ${format} files with correponding content'';
+    type        = lib.types.attrsOf type;
+    example."/hellaos.${format}".greeting  = "hello World";
     example."/hellows.${format}".greetings = [ ["Hello World"] ["Ola Mundo" ["Holla Que Tal"]]];
   };
   config.file = lib.mapAttrs toFile cfg;
--- modules/files.nix
+++ modules/files.nix
@@ -1,41 +1,38 @@
 { pkgs, config, lib, ... }:
 let
-  files = config.file;
-  fileType = (import ./file-type.nix { inherit pkgs config lib; }).fileType;
-  chmod = file:
-    if file.executable == null then ""
-    else if file.executable then "chmod +x $target"
-    else "chmod -x $target";
-  git-add = file:
-    if file.git-add == null then ""
-    else if file.git-add then "git add $target"
-    else "";
+  files     = config.file;
+  fileType  = (import ./file-type.nix { inherit pkgs config lib; }).fileType;
+  chmod     = file:
+    if file.executable == null
+      then ""
+      else if file.executable
+        then "chmod +x $target"
+        else "chmod -x $target";
+  git-add   = file:
+    if file.git-add == null
+      then ""
+      else if file.git-add
+        then "git add $target"
+        else "";
+  toName    = name: ".dsf${lib.strings.sanitizeDerivationName name}";
   # Execute this script to update the project's files
-  nameToScript = name: 
-    "devshell-files-" + (builtins.replaceStrings ["/" "."] ["-" "-"] (lib.removePrefix "/" name));
-  copy-file = name: file: pkgs.writeShellScriptBin (nameToScript name) ''
+  copy-file = name: file: pkgs.writeShellScriptBin "${toName name}" ''
     target="$PRJ_ROOT${file.target}"
     ${pkgs.coreutils}/bin/install -m 644 -D ${file.source} $target
     ${chmod file}
     ${git-add file}
   '';
-  startups = lib.mapAttrsToList (n: f: 
-    let name  = nameToScript n;
-    in { ${name}.text = ''$DEVSHELL_DIR/bin/${name}'';}
-  ) files;
+  cmd.command     = builtins.concatStringsSep "\n" startups;
+  cmd.help        = "Recreate files";
+  cmd.name        = "devshell-files";
+  opt.default     = {};
+  opt.description = "Attribute set of files to create into the project root.";
+  opt.type        = fileType "<envar>PRJ_ROOT</envar>";
+  startup.devshell-files.text = "$DEVSHELL_DIR/bin/devshell-files";
+  startups  = map (name: "source $DEVSHELL_DIR/bin/${toName name}") (builtins.attrNames files);
 in {
-  options.file = lib.mkOption {
-    description = "Attribute set of files to create into the project root.";
-    default = {};
-    type = fileType "<envar>PRJ_ROOT</envar>";
-  };
-  config.commands = [
-    {
-      name = "files";
-      help = "Regenerate files";
-      command = "nix develop --build";
-    }
-  ];
+  options.file    = lib.mkOption opt;
+  config.commands = [ cmd ];
   config.devshell.packages = lib.mapAttrsToList copy-file files;
-  config.devshell.startup = lib.foldAttrs lib.mergeAttrs {} startups;
+  config.devshell.startup  = startup;
 }
