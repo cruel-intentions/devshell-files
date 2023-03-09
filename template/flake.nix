8bcb34606a17103c5f862d52ff30fa58140b4532
Hugo Sena Ribeiro <hugosenari@gmail.com>
2023-03-09 09:12:19 -0300

    Update flake.nix

--- template/flake.nix
+++ template/flake.nix
@@ -1,10 +1,12 @@
 {
   description = "Dev Environment";
 
-  inputs.dsf.url      = "github:cruel-intentions/devshell-files";
+  inputs.dsf.url     = "github:cruel-intentions/devshell-files";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/release-22.11";
+  inputs.dsf.inputs.nixpkgs.follows = "nixpkgs";
 
   outputs = inputs: inputs.dsf.lib.shell inputs [
-    "hello"        # import nix package
+    # "hello"      # import nix package
     ./project.nix  # import nix module
   ];
 }
